31ab23d4a1ddd3778e654acd97694ad6a5a15ef8
maat <464896740@qq.com>
2024-10-30 17:53:41 +0800

    修改controller测试

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,14 +4,11 @@ class HomeController extends Controller {
   async index() {
     const { ctx } = this;
     // console.log('this.app.mysql', this.app.mysql);
-    // const results = await this.app.mysql.select('users', {
-    //   limit: 10, // 返回数据量
-    //   offset: 0, // 数据偏移量
-    // });
-    const d = new Date();
-    const hour = d.getHours();
-
-    ctx.body = hour;
+    const results = await this.app.mysql.select('users', {
+      limit: 10, // 返回数据量
+      offset: 0, // 数据偏移量
+    });
+    ctx.body = results;
   }
 }
 
