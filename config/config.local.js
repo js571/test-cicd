a7546c3d73b584d35561ab4bc98635c157266ac8
maat <464896740@qq.com>
2024-10-30 16:11:40 +0800

    增加github action进行测试

--- config/config.local.js
+++ config/config.local.js
@@ -1,23 +1,5 @@
 /* eslint valid-jsdoc: "off" */
 
-const mysql = require('mysql2');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 23306,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-});
-
-connection.connect(err => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-    return;
-  }
-  console.log('Connected to MySQL!');
-});
-
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
